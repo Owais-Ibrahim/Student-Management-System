9602cc5fd8a68210e0c02deafcc079dd070bb0c4
Owais Ibrahim <owaisibrahim@OwaisMacBookPro.local>
2021-06-15 17:32:43 -0500

    Adding teacher options

--- Main.java
+++ Main.java
@@ -1,5 +1,29 @@
+import java.util.*;
 public class Main{
     public static void main(String[] args){
-        System.out.println("HelloWorld");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Greetings are you a student or a teacher?");
+        String role = scan.next().toLowerCase();
+        if(role.equals("student"))
+            student();
+        else
+            teacher();
+    }
+
+    public static void teacher(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hey would you like to 1.Login, 2.Register, or 3.Change Password? Please enter the integer for the following choice");
+        int choice = scan.nextInt();
+        System.out.println("Please enter your username:");
+        String username = scan.next();
+        System.out.println("Please enter your password:");
+        String password = scan.next();
+        Teacher teacher = new Teacher(username, password);
+        System.out.println(teacher.getUsername());
+        System.out.println(teacher.password);
+
+    }
+    public static void student(){
+        System.out.println("Hey Student");
     }
 }
---
+++ Teacher.java
@@ -0,0 +1,19 @@
+public class Teacher {
+    String username;
+    String password;
+    public Teacher(String user, String pass){
+        username = user;
+        password = pass;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+
+    
+}
