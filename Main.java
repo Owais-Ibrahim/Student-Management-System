39d832c9f52fcfc5ee9561e326042bd78feeb8d1
Owais Ibrahim <owaisibrahim@OwaisMacBookPro.local>
2021-06-16 19:35:09 -0500

    Adding login compatability

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main{
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         System.out.println("Greetings are you a student or a teacher?");
-        String role = scan.next().toLowerCase();
+        String role = scan.nextLine().toLowerCase();
         if(role.equals("student"))
             student();
         else
@@ -12,18 +12,65 @@ public class Main{
 
     public static void teacher(){
         Scanner scan = new Scanner(System.in);
-        System.out.println("Hey would you like to 1.Login, 2.Register, or 3.Change Password? Please enter the integer for the following choice");
-        int choice = scan.nextInt();
-        System.out.println("Please enter your username:");
-        String username = scan.next();
-        System.out.println("Please enter your password:");
-        String password = scan.next();
-        Teacher teacher = new Teacher(username, password);
-        System.out.println(teacher.getUsername());
-        System.out.println(teacher.password);
-
+        int choice = 0;
+        switch (choice) {
+            case 0:
+            System.out.println("Hey would you like to \n1.Login \n2.Register\nPlease enter the integer for the following choice");
+            try {
+                choice = scan.nextInt();
+                break;
+            } catch (Exception e) {
+                System.out.println("Sorry you entered an invalid character!");
+                System.exit(1);
+            }
+            case 1:
+                //login()
+                break;
+            case 2: 
+                //register- add login info to teacher-login file and  create a teacher file where students name will be stored
+                break;
+            default:
+                System.out.println("Sorry, you inputted a different number.");
+                break;
+        }
     }
     public static void student(){
-        System.out.println("Hey Student");
+        Scanner scan = new Scanner(System.in);
+        int choice = 0;
+        switch (choice) {
+            case 0:
+            System.out.println("Hey would you like to \n1.Login \n2.Register\nPlease enter the integer for the following choice");
+            try {
+                choice = scan.nextInt();
+                break;
+            }catch (Exception e) {
+                System.out.println("Sorry you entered an invalid character!");
+                System.exit(1);
+            }
+            case 1:
+                //login()
+                System.out.println("Please enter your username?");
+                String user = scan.nextLine();
+                System.out.println("Please enter your password?");
+                String pass = scan.nextLine();
+                Teacher teacher = new Teacher(user,pass);
+                System.out.println(teacher.login());
+                break;
+            case 2: 
+                //register- add login info to student-login file and  create a student file where student's teacher and course info will be stored
+                System.out.println("Please enter your first name?");
+                String fname = scan.nextLine();
+                System.out.println("Please enter your last name?");
+                String lname = scan.nextLine();
+                System.out.println("Please enter your username?");
+                String newUser = scan.nextLine();
+                System.out.println("Please enter your password?");
+                String newPass = scan.nextLine();
+                
+                break;
+            default:
+                System.out.println("Sorry, you inputted a different number.");
+                break;
+        }
     }
 }
--- Teacher.java
+++ Teacher.java
@@ -1,11 +1,74 @@
+import java.util.ArrayList;
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.Scanner;
+
 public class Teacher {
+    ArrayList <String> userList;
+    ArrayList <String> passList;
+    
+    String firstName;
+    String lastName;
     String username;
     String password;
+   
     public Teacher(String user, String pass){
-        username = user;
-        password = pass;
+        username = user.toLowerCase();
+        password = pass.toLowerCase();
     }
 
+    public Teacher(String user, String pass, String fName, String lName){
+        firstName = fName.toLowerCase();
+        lastName = lName.toLowerCase();
+        username = user.toLowerCase();
+        password = pass.toLowerCase();
+    }
+
+    
+
+    public String login(){
+        //Getting UserList Ready
+        try {
+            File userFile = new File("teacher-user.txt");
+            Scanner reader = new Scanner(userFile);
+            while(reader.hasNextLine()){
+                userList.add(reader.nextLine());
+            }
+            reader.close();
+            
+        } catch (FileNotFoundException e) {
+            System.out.println("Sorry, file not found!");
+        }
+
+        //Getting PassList Ready
+        try {
+            File userFile = new File("teacher-pass.txt");
+            Scanner reader = new Scanner(userFile);
+            while(reader.hasNextLine()){
+                passList.add(reader.nextLine());
+            }
+            reader.close();
+            
+        } catch (FileNotFoundException e) {
+            System.out.println("Sorry, file not found!");
+        }
+
+        if(userList.contains(username)){
+            int index = userList.indexOf(username);
+            if(passList.get(index).equals(password)){
+                return "You are logged in!";
+            }
+        }
+
+        return "Sorry you have entered incorrect information.";
+    }
+
+    public String register(){
+        //Add user and pass to desingated files
+        //Create new txt file based on fname and lname
+        //Add data to the new file
+        return "Successfully Registered";
+    }
     public String getUsername(){
         return username;
     }
